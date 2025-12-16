507547b37fea69856996c3db6d9b3510fa4a59d4
Evan Perry <eperry2688@gmail.com>
2025-12-15 23:09:09 -0700

    E2EE Encrypted Media Sharing

--- frontend/src/types/module-shims.d.ts
+++ frontend/src/types/module-shims.d.ts
@@ -2,5 +2,6 @@ declare module 'expo-document-picker';
 declare module 'expo-video';
 declare module 'expo-image-manipulator';
 declare module 'expo-video-thumbnails';
+declare module 'base64-js';
 
 
